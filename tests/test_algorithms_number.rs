19ca154ef97e5185e206909f47fe8a8fb3936b60
Prajwal Tuladhar <praj@infynyxx.com>
2014-09-29 18:34:21 -0400

    add more gcd() tests

--- tests/test_algorithms_number.rs
+++ tests/test_algorithms_number.rs
@@ -3,6 +3,12 @@ use rust_algorithms::algorithms::number::gcd;
 
 #[test]
 fn test_gcd() {
-    let result = gcd(25i, 15i);
-    assert_eq!(5i, result);
+    let result1 = gcd(25i, 15i);
+    assert_eq!(5i, result1);
+
+    let result2 = gcd(4i, 2i);
+    assert_eq!(2i, result2);
+
+    let result3 = gcd(6i, 3i);
+    assert_eq!(3i, result3);
 }
