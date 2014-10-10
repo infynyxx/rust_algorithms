dd509fc110e52d3fe44aef25d030517c4d7ea3ee
Prajwal Tuladhar <praj@infynyxx.com>
2014-10-10 15:40:35 -0400

    merge sort WIP

---
+++ src/sorting/merge_sort.rs
@@ -0,0 +1,12 @@
+use sorting::Sorting;
+use utils::rand_array;
+
+pub struct MergeSort {
+    pub input_array: Vec<int>
+}
+
+impl Sorting for MergeSort {
+    fn sort(&self) -> Vec<int> {
+        
+    }
+}
